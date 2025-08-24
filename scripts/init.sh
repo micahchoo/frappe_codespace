2604d270caa69b3c6998ac7c330742910a6eaba9
Micah Alex <47268348+micahchoo@users.noreply.github.com>
2025-08-24 16:49:13 -0700

    2

--- scripts/init.sh
+++ scripts/init.sh
@@ -30,23 +30,22 @@ frappe-bench
 # Change to the newly created bench directory
 cd /workspace/frappe-bench
 
-# Configure bench to use containerized services
+# Use containers instead of localhost
 bench set-mariadb-host mariadb
 bench set-redis-cache-host redis://redis-cache:6379
 bench set-redis-queue-host redis://redis-queue:6379
 bench set-redis-socketio-host redis://redis-socketio:6379
 
-# Remove redis from Procfile as it's handled by the container
+# Remove redis from Procfile
 sed -i '/redis/d' ./Procfile
 
-# Create a new site with the correct database credentials
+# Fix errors with MariaDB connection
 bench new-site dev.localhost \
 --mariadb-root-username root \
 --mariadb-root-password 123 \
 --admin-password admin \
 --mariadb-user-host-login-scope='%'
 
-# Set developer mode and clear cache
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
 bench use dev.localhost
